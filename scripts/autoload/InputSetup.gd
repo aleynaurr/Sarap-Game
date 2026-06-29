822481d9f7f4fee7151341462a3b31cde7625ad9
tgcular <cular.tyronepro@gmail.com>
2026-06-29 23:10:48 +0800

    improved gameplay WashMinigame

--- scripts/autoload/InputSetup.gd
+++ scripts/autoload/InputSetup.gd
@@ -6,11 +6,13 @@ extends Node
 # added in _enter_tree (earliest possible point).
 
 func _enter_tree() -> void:
-	_add_action("move_up", [KEY_W, KEY_UP])
-	_add_action("move_down", [KEY_S, KEY_DOWN])
-	_add_action("move_left", [KEY_A, KEY_LEFT])
-	_add_action("move_right", [KEY_D, KEY_RIGHT])
-	_add_action("interact", [KEY_E])
+	_add_action("move_up",      [KEY_W, KEY_UP])
+	_add_action("move_down",    [KEY_S, KEY_DOWN])
+	_add_action("move_left",    [KEY_A, KEY_LEFT])
+	_add_action("move_right",   [KEY_D, KEY_RIGHT])
+	_add_action("interact",     [KEY_E])
+	_add_action("wash_faucet",  [KEY_Q, KEY_SLASH])
+	_add_action("wash_next",    [KEY_SHIFT])
 
 func _add_action(action_name: String, keys: Array) -> void:
 	if InputMap.has_action(action_name):
